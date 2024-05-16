101c223e166dc92edbfa71e981cb322a829f0c1c
SO5005 <96956061+SO5005@users.noreply.github.com>
2024-05-16 20:04:23 +0200

    Update crates/krun/src/bin/krun.rs
    Co-authored-by: Teoh Han Hui <teohhanhui@gmail.com>

--- crates/krun/src/bin/krun.rs
+++ crates/krun/src/bin/krun.rs
@@ -210,7 +210,7 @@ fn main() -> Result<()> {
 
     // https://github.com/AsahiLinux/docs/wiki/Devices
     const ASAHI_DEVS: [&str; 12] = [
-        "t8103", "t6000", "t6001", "t6002", "t8112", "t6020", "t6021", "t6022", "t8121", "t6030",
+        "t8103", "t6000", "t6001", "t6002", "t8112", "t6020", "t6021", "t6022", "t8122", "t6030",
         "t6031", "t6034",
     ];
     for key in WELL_KNOWN_ENV_VARS {
