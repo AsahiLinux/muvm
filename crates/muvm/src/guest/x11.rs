bc8867b9b164a5b7de734b9541777f1bdfc99a33
Asahi Lina <lina@asahilina.net>
2024-10-02 23:32:21 +0900

    x11: Use x112virtgpu for the secondary X11 forward if it is installed
    This lets us easily test this alternative to sommelier+XWayland without
    committing to making it the default.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- crates/muvm/src/guest/x11.rs
+++ crates/muvm/src/guest/x11.rs
@@ -2,11 +2,14 @@ use std::env;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
+use std::process::Command;
 
 use anyhow::{anyhow, Context, Result};
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 
 use super::socket::setup_socket_proxy;
+
+use crate::utils::env::find_in_path;
 
 pub fn setup_x11_forwarding<P>(run_path: P) -> Result<()>
 where
@@ -23,7 +26,17 @@ where
     }
     let host_display = &host_display[1..];
 
-    setup_socket_proxy(Path::new("/tmp/.X11-unix/X1"), 6000)?;
+    let x112virtgpu_path =
+        find_in_path("x112virtgpu").context("Failed to check existence of `x112virtgpu`")?;
+
+    if x112virtgpu_path.is_some() {
+        let mut cmd = Command::new(x112virtgpu_path.unwrap());
+        cmd.args(["--listen-display", ":1"]);
+
+        cmd.spawn().context("Failed to spawn `x112virtgpu`")?;
+    } else {
+        setup_socket_proxy(Path::new("/tmp/.X11-unix/X1"), 6000)?;
+    }
 
     // Set HOST_DISPLAY to :1, which is the display number within the guest
     // at which the actual host display is accessible.
