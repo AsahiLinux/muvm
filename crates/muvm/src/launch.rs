b706c35c3d6faee938920ee9027e41ab0a8045f7
Sergio Lopez <slp@redhat.com>
2024-10-15 23:31:52 +0200

    Implement a host memory monitor
    This monitor checks the ratio of memory available on the host and, if
    needed, contacts the VM (through muvm-server) to adjust
    /proc/sys/vm/watermark_scale_factor and, if the situation is critical,
    to request it to drop its caches.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- crates/muvm/src/launch.rs
+++ crates/muvm/src/launch.rs
@@ -148,7 +148,7 @@ fn lock_file() -> Result<(Option<File>, Uuid)> {
     Ok((Some(lock_file), cookie))
 }
 
-fn request_launch(
+pub fn request_launch(
     server_port: u32,
     cookie: Uuid,
     command: PathBuf,
--- crates/muvm/src/lib.rs
+++ crates/muvm/src/lib.rs
@@ -2,6 +2,7 @@ pub mod cli_options;
 pub mod cpu;
 pub mod env;
 pub mod launch;
+pub mod monitor;
 pub mod net;
 pub mod types;
 
